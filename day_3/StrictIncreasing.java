ebc3b79d851452f1fe9012d1405e3f509f35740e
Gowtham S <128225795+gowtham28122004@users.noreply.github.com>
2025-03-22 10:16:57 +0530

    Create StrictIncreasing.java

---
+++ day_3/StrictIncreasing.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class StrictIncreasing{
+    public static void insert(Node head){
+        Node t = head;
+        Scanner s = new Scanner(System.in);
+        Node ob = new Node();
+        while(t.next != null){
+            t = t.next;
+        }
+        ob.data = s.nextInt();
+        ob.next = null;
+        t.next = ob;
+    }
+    public static boolean strictIn(Node head){
+        Node t = head.next;
+        
+        while(t.next != null){
+            if(t.data > t.next.data){
+                return false;
+            }
+            t = t.next;
+        }
+        
+        return true;
+    }
+    public static void display(Node head){
+        Node t = head.next;
+        while(t!=null){
+            System.out.print(t.data+"->");
+            t = t.next;
+        }
+        System.out.println("Null");
+    }
+    public static void main(String[] args){
+        Node h1 = new Node();
+        h1.next = null;
+
+        
+        Node a = new Node();
+        Scanner s = new Scanner(System.in);
+        
+        int as = s.nextInt();
+        for(int i = 0;i < as;i++){
+            insert(h1);
+        }
+        
+        display(h1);
+        System.out.println(strictIn(h1));
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+}
